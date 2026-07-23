8c5936a067c94a55ed1bf8200df665d28ee8d163
Tyrannicodin <tyrannicodin@gmail.com>
2026-07-23 13:37:43 +0100

    Sample room, basic player movement

--- game/game.gd
+++ game/game.gd
@@ -1,20 +1,19 @@
 extends Node2D
 
 var rooms = [
-	preload("res://game/rooms/simple_platform.tscn"),
-	preload("res://game/rooms/second_room.tscn")
+	preload("res://resources/rooms/sample_room.tres"),
+	preload("res://resources/rooms/simple_platform.tres"),
 ]
 
 func _ready() -> void:
 	print("Hello from game!")
 	load_rooms()
 
-
 func load_rooms() -> void:
 	var initial_pos: Vector2 = Vector2.ZERO
 
 	for room in rooms:
-		var room_scene: Node2D = room.instantiate()
+		var room_scene: Node2D = room.scene.instantiate()
 
 		var entry: Marker2D = room_scene.get_node("EntryMarker")
 		var exit: Marker2D = room_scene.get_node("ExitMarker")
--- resources/room.gd
+++ resources/room.gd
@@ -1,7 +1,7 @@
 class_name RoomResource
 extends Resource
 
-## Used to load room scene
-@export var id: String
+## The room scene, must contain EntryMarker and ExitMarker
+@export var scene: PackedScene
 ## Basic probability, will likely need replaced to adjust over time
 @export var probability: float

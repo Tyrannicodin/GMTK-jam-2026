44f270b6f3c534c45e28d4cb9fcd60a18839b101
Tyrannicodin <tyrannicodin@gmail.com>
2026-07-23 02:32:17 +0100

    Basic room loading

--- game/game.gd
+++ game/game.gd
@@ -1,4 +1,27 @@
 extends Node2D
 
+var rooms = [
+	preload("res://game/rooms/simple_platform.tscn"),
+	preload("res://game/rooms/second_room.tscn")
+]
+
 func _ready() -> void:
 	print("Hello from game!")
+	load_rooms()
+
+
+func load_rooms() -> void:
+	var initial_pos: Vector2 = Vector2.ZERO
+
+	for room in rooms:
+		var room_scene: Node2D = room.instantiate()
+
+		var entry: Marker2D = room_scene.get_node("EntryMarker")
+		var exit: Marker2D = room_scene.get_node("ExitMarker")
+		if entry == null or exit == null:
+			continue
+
+		add_child(room_scene)
+
+		room_scene.position = initial_pos - entry.position
+		initial_pos += exit.position
---
+++ resources/room.gd
@@ -0,0 +1,7 @@
+class_name RoomResource
+extends Resource
+
+## Used to load room scene
+@export var id: String
+## Basic probability, will likely need replaced to adjust over time
+@export var probability: float

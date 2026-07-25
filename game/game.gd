c34e164dfe980f73dcaeeedbbdec27f0d794ceee
Benji42 <cheezyanimations@gmail.com>
2026-07-24 23:26:30 -0400

    timer styling

--- game/game.gd
+++ game/game.gd
@@ -40,6 +40,22 @@ func load_room_resources():
 func build_rooms() -> void:
 	for child in roomContainer.get_children():
 		child.queue_free()
+func set_time(time: float):
+	var s = str(float(time)).split(".")
+	
+	if s[0].length() > 1:
+		$Camera/BigText/Countdown/Character1.text = s[0][0]
+		$Camera/BigText/Countdown/Character2.text = s[0][1]
+	else:
+		$Camera/BigText/Countdown/Character2.text = s[0][0]
+	
+	$Camera/BigText/Countdown/Character3.text = s[1][0]
+
+
+func _ready() -> void:
+	print("Hello from game!")
+	load_rooms()
+	set_time(time)
 
 	var initial_pos: Vector2 = Vector2.ZERO
 	var first_room = true
@@ -118,7 +134,11 @@ func object_left_room(object: Area2D, room: Node2D, index: int):
 	#room.lock()
 
 func _physics_process(delta: float) -> void:
+<<<<<<< HEAD
 	%Timer.text = "%05.2f" % (time)
+=======
+	set_time(time)
+>>>>>>> timer styling
 
 	if countdown:
 		time -= delta

3280b324f98625e7f4779cf59e37b12448df3c59
Zunda <mailme@jacobhaussman.online>
2026-07-25 11:06:22 -0400

    dash effect

---
+++ game/player/dash_particles.gd
@@ -0,0 +1,13 @@
+extends CPUParticles2D
+
+func _ready() -> void:
+	emitting = false
+
+func emit_for_time(time: float, dash_direction: Vector2):
+	direction = dash_direction * -1
+
+	emitting = true
+
+	await get_tree().create_timer(time).timeout
+
+	emitting = false

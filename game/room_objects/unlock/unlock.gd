ac82970f3dfd739fe0d6703262fc1edd6d5ee298
Benji42 <cheezyanimations@gmail.com>
2026-07-26 10:55:49 -0400

    final changes

--- game/room_objects/unlock/unlock.gd
+++ game/room_objects/unlock/unlock.gd
@@ -11,7 +11,7 @@ func set_unlock(new_unlock: UnlockResource) -> void:
 	%Keybind.text = unlock.keybind
 	%Cooldown.text = "Cooldown: " + unlock.cooldown
 	
-	var colorOverride = StyleBoxFlat.new()
-	colorOverride.bg_color = unlock.color
-	remove_theme_stylebox_override("panel")
-	add_theme_stylebox_override("panel", colorOverride)
+	#var colorOverride = StyleBoxFlat.new()
+	#colorOverride.bg_color = unlock.color
+	#remove_theme_stylebox_override("panel")
+	#add_theme_stylebox_override("panel", colorOverride)

d833d21dd0b00aaa78e1d5ebe78bbd3a0dbabba5
Rvtar <shinboon.boon@gmail.com>
2026-07-27 03:22:52 +0700

    Fix Upgrade Selector eating Dash Input

--- game/player/player.gd
+++ game/player/player.gd
@@ -319,7 +319,7 @@ func _physics_process(delta):
 
 	for area in detector.get_overlapping_areas():
 		if area.get_parent().get("unlock"):
-			if %InputBuffer.is_just_pressed("dash"):
+			if Input.is_action_just_pressed("dash"):
 				unlock_selected.emit(area.get_parent().get("unlock"))
 	
 	# Handle jump.
